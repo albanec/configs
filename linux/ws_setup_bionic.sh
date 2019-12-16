82a265f2f57d861c2d71022088f4c11f8580fdd8
Evgeniy <eugeni.maksimov@yandex.ru>
2019-12-16 04:31:09 +0300

    Update ws_setup_bionic.sh

--- linux/ws_setup_bionic.sh
+++ linux/ws_setup_bionic.sh
@@ -1,14 +1,17 @@
 # установить terminator и запускать в нём!
-sudo apt-get update
-sudo apt-get remove -y gnome-mines gnome-sudoku sgt-puzzles gnome-software xfburn xfce4-notes orage parole \
-libreoffice-* ristretto pidgin update-notifier numix-gtk-theme simple-scan xfce4-dict xfce4-weather-plugin \
-xfce4-cpugraph-plugin xterm xfce4-terminal mousepad
+sudo apt update
+sudo apt remove -y gnome-mines gnome-sudoku sgt-puzzles gnome-software xfburn xfce4-notes orage parole \
+    libreoffice-math \
+    ristretto pidgin update-notifier numix-gtk-theme simple-scan xfce4-dict xfce4-weather-plugin \
+    xfce4-cpugraph-plugin xterm xfce4-terminal mousepad
 #
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
 #
-sudo apt-get install -y keepassx terminator plank openvpn screenfetch curl numix-icon-theme p7zip-full p7zip-rar \
-xubuntu-restricted-extras qemu-kvm libvirt-bin bridge-utils ppa-purge
+sudo apt install -y chromium-browser keepassxc terminator plank openvpn screenfetch curl numix-icon-theme p7zip-full p7zip-rar nmap \
+	gnome-system-monitor \
+    xubuntu-restricted-extras qemu-kvm libvirt-bin bridge-utils ppa-purge \
+    software-properties-common apt-transport-https wget
 #
 mkdir ~/Templates/gear
 wget -O- https://telegram.org/dl/desktop/linux | sudo tar xJ -C ~/Templates/gear
@@ -16,23 +19,29 @@ sudo ln -s ~/Templates/gear/Telegram/Telegram /usr/local/bin/telegram-desktop
 #
 wget https://download.truecrypt.ch/current/truecrypt-7.1a-linux-x64.tar.gz
 tar xvf truecrypt-7.1a-linux-x64.tar.gz
-sudo ./truecrypt-7.1a-linux-x64
+sudo ./truecrypt-7.1a-setup-x64
+rm truecrypt-7.1a-linux-x64.tar.gz truecrypt-7.1a-setup-x64
 #
 #
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q    https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q    https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+
 #
-sudo apt-add-repository -y "deb https://download.sublimetext.com/ apt/stable/" && \
+sudo     apt-add-repository -y "deb https://download.sublimetext.com/ apt/stable/" && \
     sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge" && \
-    sudo add-apt-repository -y "deb https://download.virtualbox.org/virtualbox/debian bionic contrib" && \
+    sudo add-apt-repository -y "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian bionic contrib" && \
+    sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" && \
     sudo add-apt-repository -y ppa:numix/ppa && \
     sudo add-apt-repository -y ppa:libreoffice/ppa
-sudo apt-get update
+
+sudo apt update
 #
-sudo apt install -y sublime-text docker-ce docker-compose virtualbox-6.0 numix-gtk-theme numix-icon-theme-circle \
-	libreoffice-writer libreoffice-calc libreoffice-draw libreoffice-impress libreoffice-gtk libreoffice-style-elementary \
-	pulseaudio-equalizer \
+sudo apt install -y sublime-text docker-ce docker-compose virtualbox-6.0 numix-gtk-theme numix-icon-theme-circle remmina network-manager-openconnect-gnome\
+	# libreoffice-draw libreoffice-impress libreoffice-gtk \
+	# pulseaudio-equalizer \
+	code \
 	fonts-firacode fonts-hack
 # sudo usermod -aG docker ${USER}
 #
@@ -43,7 +52,7 @@ sudo chmod 755 /usr/share/plank/themes/* -R
 rm Plank-Themes-master -r
 rm Plank-Themes-master.zip
 #
-sudo sh -c "echo 'vm.swappiness = 10\n\
+sudo sh -c "echo 'vm.swappiness = 30\n\
 net.ipv4.conf.all.accept_redirects = 0\n\
 net.ipv6.conf.all.accept_redirects = 0\n\
 net.ipv4.conf.all.secure_redirects = 0\n\
@@ -54,13 +63,11 @@ net.ipv4.tcp_syncookies = 1\n\
 net.ipv4.tcp_timestamps = 0\n\
 net.ipv4.conf.all.rp_filter = 1\n\
 net.ipv4.conf.lo.rp_filter = 1\n\
-net.ipv4.conf.wlp3s0.rp_filter = 1\n\
-net.ipv4.conf.enp0s25.rp_filter = 1\n\
+net.ipv4.conf.wlp2s0.rp_filter = 1\n\
 net.ipv4.conf.default.rp_filter = 1\n\
 net.ipv4.conf.all.accept_source_route = 0\n\
 net.ipv4.conf.lo.accept_source_route = 0\n\
 net.ipv4.conf.wlp3s0.accept_source_route = 0\n\
-net.ipv4.conf.enp0s25.accept_source_route = 0\n\
 net.ipv4.conf.default.accept_source_route = 0\n\
 net.ipv4.tcp_window_scaling = 1\n\
 net.ipv4.icmp_echo_ignore_broadcasts = 1\n\
@@ -75,19 +82,19 @@ sudo sysctl -p
 #
 sudo ufw enable
 #
-sudo sh -c "echo '[connection]\n\
-wifi.powersave = 2' > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf"
-echo "options iwlwifi 11n_disable=8" | sudo tee /etc/modprobe.d/iwlwifi11n.conf
+#sudo sh -c "echo '[connection]\n\
+#wifi.powersave = 2' > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf"
+#echo "options iwlwifi 11n_disable=8" | sudo tee /etc/modprobe.d/iwlwifi11n.conf
 #
-sudo sh -c "echo 'default-sample-format = float32le\n\
+sudo sh -c "echo 'default-sample-format = float32ne\n\
 default-sample-rate = 48000\n\
 alternate-sample-rate = 44100\n\
 default-sample-channels = 2\n\
 default-channel-map = front-left,front-right\n\
 default-fragments = 2\n\
 default-fragment-size-msec = 125\n\
-resample-method = soxr-vhq\n\
-#resample-method = copy\n\
+# resample-method = speex-float-5\n\
+resample-method = copy\n\
 enable-lfe-remixing = no\n\
 high-priority = yes\n\
 nice-level = -11\n\
@@ -96,10 +103,19 @@ realtime-priority = 9\n\
 rlimit-rtprio = 9\n\
 daemonize = no' >> /etc/pulse/daemon.conf"
 sudo sh -c "echo 'defaults.pcm.minperiodtime 0\n\
-defaults.pcm.rate_converter samplerate_best\n\
+defaults.pcm.rate_converter speexrate_medium\n\
 defaults.pcm.dmix.rate 44100' >> /usr/share/alsa/alsa.conf"
 sudo sh -c "# Use PulseAudio plugin hw\n\
 pcm.!default {\n\
    type plug\n\
    slave.pcm hw\n\
 }' >> /etc/asound.conf"
+
+
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt dist-upgrade
+sudo apt install nvidia-driver-440
+
+sudo -i
+echo 5 > /proc/sys/vm/laptop_mode
+exit
